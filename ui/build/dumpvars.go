39fda039eafbd816f3d01cfce90bf288a9419ca2
Jody Yuantoro <xyzuannihboss@gmail.com>
2021-10-08 12:38:13 +0700

    soong: dumpvars: ClownUI dumpvars display
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -145,39 +145,39 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 
 // Variables to print out in the top banner
 var BannerVars = []string{
-	"PLATFORM_VERSION_CODENAME",
 	"PLATFORM_VERSION",
-	"PRODUCT_INCLUDE_TAGS",
-	"PRODUCT_SOURCE_ROOT_DIRS",
 	"CLOWN_VERSION",
+	"BUILD_ID",
 	"TARGET_PRODUCT",
 	"TARGET_BUILD_VARIANT",
-	"TARGET_BUILD_APPS",
 	"TARGET_BUILD_UNBUNDLED",
 	"TARGET_ARCH",
 	"TARGET_ARCH_VARIANT",
 	"TARGET_CPU_VARIANT",
-	"TARGET_2ND_ARCH",
-	"TARGET_2ND_ARCH_VARIANT",
-	"TARGET_2ND_CPU_VARIANT",
-	"HOST_OS",
-	"HOST_OS_EXTRA",
 	"HOST_CROSS_OS",
-	"BUILD_ID",
-	"OUT_DIR",
+	"PRODUCT_INCLUDE_TAGS",
+	"PRODUCT_SOURCE_ROOT_DIRS",
 	"SOONG_SDK_SNAPSHOT_TARGET_BUILD_RELEASE",
 }
 
 func Banner(make_vars map[string]string) string {
 	b := &bytes.Buffer{}
 
-	fmt.Fprintln(b, "============================================")
+	fmt.Fprintln(b, "==========================================================")
+    fmt.Fprintln(b, " ██████╗██╗      ██████╗ ██╗    ██╗███╗   ██╗██╗   ██╗██╗ ")
+    fmt.Fprintln(b, "██╔════╝██║     ██╔═══██╗██║    ██║████╗  ██║██║   ██║██║ ")
+	fmt.Fprintln(b, "██║     ██║     ██║   ██║██║ █╗ ██║██╔██╗ ██║██║   ██║██║ ")
+	fmt.Fprintln(b, "██║     ██║     ██║   ██║██║███╗██║██║╚██╗██║██║   ██║██║ ")
+	fmt.Fprintln(b, "╚██████╗███████╗╚██████╔╝╚███╔███╔╝██║ ╚████║╚██████╔╝██║ ")
+	fmt.Fprintln(b, " ╚═════╝╚══════╝ ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝ ╚═════╝ ╚═╝ ")
+	fmt.Fprintln(b, "==========================================================")
+
 	for _, name := range BannerVars {
 		if make_vars[name] != "" {
 			fmt.Fprintf(b, "%s=%s\n", name, make_vars[name])
 		}
 	}
-	fmt.Fprint(b, "============================================")
+	fmt.Fprintln(b, "==========================================================")
 
 	return b.String()
 }
