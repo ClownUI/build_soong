851e27ba83aa60256097d3c59a53c0af82509786
Nolen Johnson <johnsonnolen@gmail.com>
2022-02-17 13:25:13 -0500

    Add dumpvars for GMS
    * For most people and in most enviorments these will
      never pop up, but given we now support GMS variants
      and different build types we ought to tell the user
      which one is in use.
    
    Change-Id: I7639f2e741d9b24e3719ef76d7d532c0155cd988
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -159,7 +159,12 @@ var BannerVars = []string{
 	"HOST_OS",
 	"SOONG_SDK_SNAPSHOT_PREFER",
 	"SOONG_SDK_SNAPSHOT_TARGET_BUILD_RELEASE",
+	"PRODUCT_IS_ATV",
+	"PRODUCT_IS_AUTOMOTIVE",
 	"WITH_SU",
+	"WITH_GMS",
+	"GMS_MAKEFILE",
+	"MAINLINE_MODULES_MAKEFILE",
 }
 
 func Banner(make_vars map[string]string) string {
