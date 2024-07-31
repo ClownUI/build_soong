3990a70a011ee019469a6f59534fb8cda99f9260
Inseob Kim <inseob@google.com>
2024-07-31 02:00:41 +0000

    Revert^6 "Add TARGET_SYSTEM_PROP to system build.prop"
    530c32acbee136ff3475192335cc7594589de096
    
    Change-Id: I9dc817855e3db7b96320a977b24b84d72e762893
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -534,6 +534,8 @@ type ProductVariables struct {
 
 	ArtTargetIncludeDebugBuild *bool `json:",omitempty"`
 
+	SystemPropFiles []string `json:",omitempty"`
+
 	ClownDevice []string `json:",omitempty"`
 }
 
